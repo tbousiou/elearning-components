a094aa9b04d922bb30b6afff8abc08a47b0d1f83
Theodoros Bousiou <tbousiou@gmail.com>
2025-07-29 19:12:00 +0300

    remove flipcards from css

--- combine-css.ps1
+++ combine-css.ps1
@@ -6,7 +6,6 @@ Get-Content "general.css" | Out-File -FilePath "style.css" -Append -Encoding UTF
 "/* Components Styles */" | Out-File -FilePath "style.css" -Append -Encoding UTF8
 Get-Content "components.css" | Out-File -FilePath "style.css" -Append -Encoding UTF8
 "" | Out-File -FilePath "style.css" -Append -Encoding UTF8
-"/* Flipcards Styles */" | Out-File -FilePath "style.css" -Append -Encoding UTF8
-Get-Content "flipcards.css" | Out-File -FilePath "style.css" -Append -Encoding UTF8
+
 
 Write-Host "CSS files combined successfully!" -ForegroundColor Green
